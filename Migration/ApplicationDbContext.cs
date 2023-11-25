b51ec2359dcbf2b92a4ab36b9a4d83fd7bbb1567
DiegoCortez04 <c.diego2004@gmail.com>
2023-11-25 13:44:30 -0500

    Correcciones
    Cambios

--- Migration/ApplicationDbContext.cs
+++ Migration/ApplicationDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace ScireHub.Migration
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ScireHub.Migration
 {
     public class ApplicationDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
     }
 }
